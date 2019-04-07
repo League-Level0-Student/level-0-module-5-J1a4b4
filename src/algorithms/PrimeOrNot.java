79ed300e2264f9fa4716bf7f06646c9bad6e401a
elladem <47702293+zzmimicoder@users.noreply.github.com>
2019-04-07 13:00:28 -0700

    PrimeOrNot

---
+++ src/algorithms/PrimeOrNot.java
@@ -0,0 +1,21 @@
+package algorithms;
+
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+		String num = JOptionPane.showInputDialog(null, "Please enter a number.");
+int number = Integer.parseInt(num);
+int divisor = 2;
+for (int i = 0; i < number - 2; i++) {
+	if (number % divisor == 0) {
+		JOptionPane.showMessageDialog(null, "Your number, " + number + ", is not a prime number.");
+		break;
+	}
+	divisor = divisor + 1;
+}
+JOptionPane.showMessageDialog(null, "Your number, " + number + ", is a prime number.");
+	}
+
+}
